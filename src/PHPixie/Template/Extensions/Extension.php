2d1fb9b7d1c6d005c596e4fd22fd9ecd8b8acded
Dracony <draconyster@gmail.com>
2016-02-11 14:05:21 +0100

    interface docblocks

--- src/PHPixie/Template/Extensions/Extension.php
+++ src/PHPixie/Template/Extensions/Extension.php
@@ -2,9 +2,26 @@
 
 namespace PHPixie\Template\Extensions;
 
+/**
+ * Extension interface
+ */
 interface Extension
 {
+    /**
+     * Extension name
+     * @return string
+     */
     public function name();
+
+    /**
+     * Map of methods that should be available in templates.
+     * @return array
+     */
     public function methods();
+
+    /**
+     * Map of method aliases
+     * @return array
+     */
     public function aliases();
 }
--- src/PHPixie/Template/Formats/Format.php
+++ src/PHPixie/Template/Formats/Format.php
@@ -2,8 +2,21 @@
 
 namespace PHPixie\Template\Formats;
 
+/**
+ * Format interface
+ */
 interface Format
 {
+    /**
+     * Array of file extensions that this format supports
+     * @return array
+     */
     public function handledExtensions();
+
+    /**
+     * Compile a file into a PHP template
+     * @param string $file File name
+     * @return string
+     */
     public function compile($file);
 }
